d28f8bd46c3b7ae22d70420ab6699fd1552d3801
Heracles <971jordan@live.fr>
2019-01-10 16:50:33 +0100

    Add menu

---
+++ scripts/menu.py
@@ -0,0 +1,11 @@
+print("                            _   _       _      ")
+print("  __ _  __ _ _ __ ___   ___| | | |_ __ (_)_  __")
+print(" / _` |/ _` | '_ ` _ \ / _ \ | | | '_ \| \ \/ /")
+print("| (_| | (_| | | | | | |  __/ |_| | | | | |>  < ")
+print(" \__, |\__,_|_| |_| |_|\___|\___/|_| |_|_/_/\_\ ")
+print(" |___/   ")
+
+print("Press Enter to start the minesweeper")
+input()
+import minesweeper
+minesweeper.playgame()
